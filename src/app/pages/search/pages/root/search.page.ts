bc114abc3a0ad75e56e470011b08d31aca550292
daftclaud <cdsmileyone@gmail.com>
2020-05-20 16:41:21 -0500

    got next and last page number

--- src/app/pages/search/pages/root/search.page.ts
+++ src/app/pages/search/pages/root/search.page.ts
@@ -8,25 +8,28 @@ import { take } from 'rxjs/operators';
   styleUrls: ['./search.page.scss'],
 })
 export class SearchPage implements OnInit {
-
   resultCount: number;
   remainingRequests: number;
   results: GitUser[];
 
-  constructor(
-    private githubSvc: GithubService
-  ) { }
+  constructor(private githubSvc: GithubService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   async search(query: string) {
-    const res = await this.githubSvc.searchUsers(query).pipe(take(1)).toPromise();
+    /*
+      To-do:
+        - add try/catch
+        - paginate
+    */
+    const regex = /page=[0-9]+/g;
+    const res = await this.githubSvc
+      .searchUsers(query)
+      .pipe(take(1))
+      .toPromise();
     this.remainingRequests = +res.headers.get('X-RateLimit-Remaining');
     this.resultCount = (res.body as any).total_count;
     this.results = (res.body as any).items;
-    console.log('remaing requests: ', res.headers.get('X-RateLimit-Remaining'));
-    console.log('body: ', res.body);
+    const [next, last] = res.headers.get('link').match(regex).map(page => page.split('=')[1]);
   }
-
 }
--- src/app/shared/services/github.service.ts
+++ src/app/shared/services/github.service.ts
@@ -40,7 +40,7 @@ export class GithubService {
 
   constructor(private http: HttpClient) {}
 
-  searchUsers(query: string) {
+  searchUsers(query: string, page?: number) {
     const url = this.baseURL + `/search/users?q=${query}`;
     return this.http.get(url, { observe: 'response' });
   }
