b95248279f9a3319ba6a355a085d1309ffaddb08
daftclaud <cdsmileyone@gmail.com>
2020-05-23 12:23:04 -0500

    refactored basic pagination

--- src/app/pages/search/components/pagination/pagination.component.ts
+++ src/app/pages/search/components/pagination/pagination.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
@@ -8,8 +8,13 @@ import { Component, OnInit, Input } from '@angular/core';
 export class PaginationComponent implements OnInit {
 
   @Input() numItems: number;
+  @Input() itemTotal: number;
   @Input() itemsPerPage: number;
   @Input() stepSize: number;
+  // To-do: Add input for scroll events subject
+  //                               page, itemsToGet, addToFront
+  @Output() getMore: EventEmitter<[number, number,  boolean]> = new EventEmitter();
+  pagesNavigated: number[];
   currentPage: number;
   lastPage: number;
   bucketIndex: number;
@@ -20,20 +25,37 @@ export class PaginationComponent implements OnInit {
   ngOnInit() {
     this.currentPage = 1;
     this.bucketIndex = 0;
-    this.lastPage = Math.ceil(this.numItems / this.itemsPerPage);
+    this.pagesNavigated = [0];
+    this.lastPage = Math.ceil(this.itemTotal / this.itemsPerPage);
     this.lastBucketIndex = Math.ceil(this.lastPage / this.stepSize) - 1;
   }
 
-  previous() {
-    console.log('To-do: implement previous');
+  previousPage() {
+    this.currentPage--;
+    if (!this.pagesNavigated.includes(this.currentPage)) {
+      this.pagesNavigated.push(this.currentPage);
+    }
+    if (this.shouldFetch(this.currentPage)) {
+      this.getMore.emit([this.currentPage, this.itemsPerPage, true]);
+    }
   }
 
-  next() {
-    console.log('To-do: implement next');
+  nextPage() {
+    this.currentPage++;
+    if (!this.pagesNavigated.includes(this.currentPage)) {
+      this.pagesNavigated.push(this.currentPage);
+    }
+    if (this.shouldFetch(this.currentPage)) {
+      this.getMore.emit([this.currentPage, this.itemsPerPage, false]);
+    }
   }
 
   goToPage() {
     console.log('To-do: implement goToPage');
+  }
+
+  private shouldFetch(page: number) {
+    return this.pagesNavigated.includes(page);
   }
 
 }
